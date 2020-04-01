a1fabfa065ef7ebaa00702bf1ef1b6dc977a6d78
Carlos21122000 <craxtum1@gmail.com>
2020-04-01 16:57:51 -0600

    carga

--- AppLigaNacional/app/src/main/java/com/example/appliganacional/ui/home/HomeFragment.kt
+++ AppLigaNacional/app/src/main/java/com/example/appliganacional/ui/home/HomeFragment.kt
@@ -8,24 +8,30 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.appliganacional.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
-
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
+        return inflater.inflate(R.layout.fragment_home, container, false)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
         })
         return root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonRegis.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_home_to_registro)
+        }
+
+    }
+
 }
