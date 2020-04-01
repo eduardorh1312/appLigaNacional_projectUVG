2cdc8a8b185b922f611a2351abcfcf37d093762a
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2020-04-01 00:51:24 -0600

    Metodo de enviar correo

--- AppLigaNacional/app/src/main/java/com/example/appliganacional/ui/PagoFragment.kt
+++ AppLigaNacional/app/src/main/java/com/example/appliganacional/ui/PagoFragment.kt
@@ -11,6 +11,7 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.navigation.Navigation
 import com.example.appliganacional.R
+import kotlinx.android.synthetic.main.fragment_pago.*
 
 
 class PagoFragment : Fragment() {
@@ -18,5 +19,27 @@ class PagoFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pago, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val correo = view!!.findViewById<TextView>(R.id.email_s)
+
+        pagar_final.setOnClickListener {
+            sendMail()
+        }
+    }
+
+    fun sendMail() {
+        val correo = view!!.findViewById<TextView>(R.id.email_s)
+        var recipient = correo.getText().toString()
+        var recipientsList = arrayListOf<String>()
+        recipientsList.add(recipient)
+        var intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_EMAIL, recipientsList)
+        //intent.putExtra(Intent.EXTRA_TEXT, message)
+        intent.setType("message/rfc822")
+        startActivity(Intent.createChooser(intent, "Choose an email client"))
     }
 }
