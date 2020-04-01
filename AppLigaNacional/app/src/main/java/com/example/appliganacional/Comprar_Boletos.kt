c113a4b9399f8359219ad4e56212652e00d551b6
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2020-04-01 00:10:26 -0600

    Minimo cambio

--- AppLigaNacional/app/src/main/java/com/example/appliganacional/Comprar_Boletos.kt
+++ AppLigaNacional/app/src/main/java/com/example/appliganacional/Comprar_Boletos.kt
@@ -22,14 +22,18 @@ class Comprar_Boletos : Fragment() {
 
         val total_ninos = view!!.findViewById<TextView>(R.id.total_ninos)
         val total_adultos = view!!.findViewById<TextView>(R.id.total_adultos)
+        val total = view!!.findViewById<TextView>(R.id.total)
         var t_kids = 0;
         var t_adultos = 0;
+        var t_total = 0;
 
 
         restar_kids.setOnClickListener {
             if(t_kids <= 6 && t_kids > 0){
-                t_kids = t_kids - 1;
+                t_kids = t_kids - 1
                 total_ninos.setText(t_kids)
+                t_total = t_total - 12
+                total.setText("Total: Q"+ t_total)
             }
         }
 
@@ -37,6 +41,8 @@ class Comprar_Boletos : Fragment() {
             if(t_kids < 6 && t_kids >= 0){
                 t_kids = t_kids + 1;
                 total_ninos.setText(t_kids)
+                t_total = t_total + 12
+                total.setText("Total: Q"+ t_total)
             }
         }
 
@@ -44,6 +50,8 @@ class Comprar_Boletos : Fragment() {
             if(t_adultos <= 6 && t_kids > 0){
                 t_adultos = t_adultos - 1;
                 total_adultos.setText(t_adultos)
+                t_total = t_total - 20
+                total.setText("Total: Q"+ t_total)
             }
         }
 
@@ -51,6 +59,8 @@ class Comprar_Boletos : Fragment() {
             if(t_adultos < 6 && t_kids >= 0){
                 t_adultos = t_adultos + 1;
                 total_adultos.setText(t_adultos)
+                t_total = t_total + 20
+                total.setText("Total: Q"+ t_total)
             }
         }
         pagar1.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_comprar_Boletos_to_pagoFragment))
