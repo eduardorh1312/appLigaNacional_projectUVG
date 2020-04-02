06727ce0380a3ae9873843edd2aff0fde05db41f
eduardorh1312 <53280540+eduardorh1312@users.noreply.github.com>
2020-04-01 20:15:22 -0600

    Commit

--- AppLigaNacional/app/src/main/java/com/example/appliganacional/Clasificacion.kt
+++ AppLigaNacional/app/src/main/java/com/example/appliganacional/Clasificacion.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_clasificacion.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,11 @@ class Clasificacion : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_clasificacion, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_clasificacion_to_nav_home))
     }
 
     companion object {
