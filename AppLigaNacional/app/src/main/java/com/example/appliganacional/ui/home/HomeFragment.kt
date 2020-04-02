2b2824a2c939c0f2530396e136ef56f6d3258598
Carlos21122000 <craxtum1@gmail.com>
2020-04-02 01:07:14 -0600

    Carlos

--- AppLigaNacional/app/src/main/java/com/example/appliganacional/ui/home/HomeFragment.kt
+++ AppLigaNacional/app/src/main/java/com/example/appliganacional/ui/home/HomeFragment.kt
@@ -10,32 +10,45 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.example.appliganacional.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
-
-<<<<<<< HEAD
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-=======
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
->>>>>>> e64105d96d7af81ede3f1b656e4de45aa0ba3c7f
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         return inflater.inflate(R.layout.fragment_home, container, false)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
 
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
         })
         return root
     }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+/*
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
         buttonRegis.setOnClickListener {
             findNavController().navigate(R.id.action_nav_home_to_registro)
         }
+        buttonIngr.setOnClickListener {
+            //startActivity(intent(this.fragment_registro::class.java))
+            //finish()
+        }
+    }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun updateUI(currenteUser: FirebaseUser?){
 
     }
 
+ */
 }
